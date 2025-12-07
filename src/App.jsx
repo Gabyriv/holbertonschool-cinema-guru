0fba7449b66cfda7e7e62d42f3ed21fd76fc62da
Gabriel <gabyrivm06@gmail.com>
2025-12-07 17:36:27 -0400

    feat: Initialize the main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
+import { useState, useEffect } from "react"
+import "./App.css"
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userUsername, setUserUsername] = useState("")
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken")
+    if (!accessToken) return
+
+    fetch("/api/auth/", {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) throw new Error("Auth failed")
+        return res.json()
+      })
+      .then(data => {
+        setIsLoggedIn(true)
+        setUserUsername(data.username)
+      })
+      .catch(() => {
+        setIsLoggedIn(false)
+        setUserUsername("")
+      })
+  }, [])
+
+
   return (
     <div className="App">
+      {isLoggedIn ? (
+        // if true, The Dashboard component should be rendered (Will be built in later tasks)
+        <></>
+      ) : (
+        // if false, The Authentication component shoule be rendered (Will be built in later tasks)
+        <></>
+      )}
     </div>
   )
 }
